ecb64e02072802c824895f4d641182f715fa08a6
Nguyen Trung <nguyentrung250302@gmail.com>
2022-09-29 22:54:05 +0700

    day len

---
+++ lesson_one/firstcode.js
@@ -0,0 +1,19 @@
+var fullName = 'Chao mung ban den voi trang web cua toi !';
+
+alert(fullName); 
+
+confirm('hãy đảm đảo bạn trên 18 tuổi!');
+
+prompt('xac nhan độ tuổi của bạn để tham gia ');
+
+setTimeout (function()  {
+  alert('thong bao, ban du tieu chuan tham gia !') 
+},2000);
+
+setInterval (function()  {
+  console.log('hay dang nhap lại ! ' + Math.random ())
+},5000);
+/**bài học đầu tiên làm quen với web, học hiển thị thông báo,
+ *  nội dung hộp thoại trên trang web tự tạo. thông qua extension "Go live" 
+ * liên kết trên chrome để thực hiện 
+ */
